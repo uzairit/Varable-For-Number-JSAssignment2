5e8d26c3b8a648bd15adb4f72ecf7a742ce8ee25
Uzair Raza <uzair18it@gmail.com>
2024-10-03 04:57:04 +0500

    Assignment 2

---
+++ myscript.js
@@ -0,0 +1,11 @@
+var age="I am 18 years old";
+alert(age);
+
+var vt="You have visited this site 14 times";
+alert(vt)
+
+document.write("<br>")
+document.write("My birth year is 2005");
+document.write("<br>")
+document.write("Ubaid ordered 5 t-shirt on xyz website")
+
